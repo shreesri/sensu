32d2c767466b49991002c99562f7ebd2ac72c693
shreesri <sreedhar.unix@gmail.com>
2018-01-22 18:34:53 +0530

    Update shree_variable_replace.sh

--- shree_variable_replace.sh
+++ shree_variable_replace.sh
@@ -2,13 +2,13 @@
 
 targetDir="/etc/sensu/conf.d/"
 searstring="Hostname"
-repleacestring=$1
+REPLACESTRING=$1
 if [ -d $targetDir ];
 then
         cd $targetDir
         for file in $(grep -il $searstring test*)
         do
-                sed -e "s/$searstring/$repleacestring/ig" $file > /tmp/tempfile.tmp
+                sed -e "s/$searstring/$REPLACESTRING/ig" $file > /tmp/tempfile.tmp
                 mv /tmp/tempfile.tmp $file
         done
 fi
